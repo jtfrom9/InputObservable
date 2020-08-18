f5e71dd4b9d4d82e1bf0be6cac9e287bbc1a11fc
jtfrom9 <jun.tachikawa@gmail.com>
2020-08-18 20:16:51 +0900

    fix ObjectViewer

--- Assets/Example/Scripts/ObjectViewer.cs
+++ Assets/Example/Scripts/ObjectViewer.cs
@@ -7,44 +7,49 @@ using InputObservable;
 
 public class ObjectViewer : MonoBehaviour
 {
-    public Transform target;
+    public GameObject prefab;
+    Transform target;
+
+    void Awake()
+    {
+        target = Instantiate(prefab).transform;
+    }
 
     void Start()
     {
-        // var gio = new GyroInputObservable(this);
-        // gio.EulerAngles.Subscribe(e => {
-        //     target.rotation = Quaternion.Euler(e);
-        // }).AddTo(this);
         var touch0 = this.DefaultInputObservable(0, null);
         var touch1 = this.DefaultInputObservable(1, null);
 
         touch0.Any().Where(e => e.type != InputEventType.Move).Subscribe(e => { Debug.Log(e); }).AddTo(this);
         touch1.Any().Where(e => e.type != InputEventType.Move).Subscribe(e => { Debug.Log(e); }).AddTo(this);
 
-        touch0.ToEulerAngle(new Vector2 { x = -180, y = -90 })
+        // Rotate by Touch Operation
+        touch0.ToEulerAngle(new Vector2 { x = -180, y = -180 })
             .Where(_ => !touch1.Began)
             .Subscribe(rot =>
             {
-                // gio.AddRotate(rot);
-                target.Rotate(rot);
+                target.Rotate(rot, Space.World);
             }).AddTo(this);
 
+        // Reset
         var orig_rotate = target.rotation;
         var orig_scale = target.localScale;
         touch0.DoubleSequence(200).Subscribe(_ =>
         {
-            Debug.Log("reset");
             target.rotation = orig_rotate;
             target.localScale = orig_scale;
         }).AddTo(this);
 
+        // Scale Object by Mouse Wheel
         var camera = Camera.main;
         (touch0 as IMouseWheelObservable)?.Wheel.Subscribe(v =>
         {
-            // camera.transform.Translate(Vector3.forward * v.wheel);
-            target.localScale += Vector3.one * v.wheel;
+            var scale = target.localScale + Vector3.one * v.wheel;
+            if(0.1 < scale.x && scale.x < 10)
+                target.localScale += Vector3.one * v.wheel;
         }).AddTo(this);
 
+        // Scale Object by Pinch Operation
         RectangleObservable.From(touch0, touch1)
             .PinchSequence()
             .RepeatUntilDestroy(this)
@@ -53,14 +58,22 @@ public class ObjectViewer : MonoBehaviour
             if (diff.x > 0 && diff.y > 0)
             {
                 var v = Mathf.Max(diff.x / Screen.width, diff.y / Screen.height);
-                target.localScale += Vector3.one * v * 10;
-                Debug.Log($"pinch-out: diff={diff}, v={v}, localScale={target.localScale}");
+                var scale = target.localScale + Vector3.one * v * 10;
+                if (scale.x < 10)
+                {
+                    target.localScale = scale;
+                    Debug.Log($"pinch-out: diff={diff}, v={v}, localScale={target.localScale}");
+                }
             }
             else if (diff.x < 0 && diff.y < 0)
             {
                 var v = Mathf.Min(diff.x / Screen.width, diff.y / Screen.height);
-                target.localScale += Vector3.one * v * 10;
-                Debug.Log($"pinch-in: diff={diff}, v={v}, localScale={target.localScale}");
+                var scale = target.localScale + Vector3.one * v * 10;
+                if (scale.x > 0.1)
+                {
+                    target.localScale = scale;
+                    Debug.Log($"pinch-in: diff={diff}, v={v}, localScale={target.localScale}");
+                }
             }
         }).AddTo(this);
     }
